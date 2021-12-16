4e13ca0138f0192ebb0240c51f103cefb36adc24
sumana10 <sumanaonlyme@gmail.com>
2021-12-16 12:21:09 +0530

    redux demo

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const redux = require('redux')
+const createstore = redux.createStore
+const combineReducers = redux.combineReducers
 // console.log("Redux demo");
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 //Action is an js object with type property
 
@@ -13,13 +16,42 @@ function buyCake(){
     info: 'First redux action'
   }
 }
-
-const initialState = {
-  numOfCakes: 10
+function buyIceCream(){
+  return {
+    type: BUY_ICECREAM,
+    info: 'First redux action'
+  }
 }
 
-const reducer = (state = initialState, action) =>{
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCream: 20
+// }
+const initialCakeState = {
+  numOfCakes: 10
+}
+const initialIceCreamState = {
+  numOfIceCream: 20
+}
+// const reducer = (state = initialState, action) =>{
 
+//   switch(action.type){
+
+//     case BUY_CAKE:
+//       return{
+//         ...state,
+//         numOfCakes: state.numOfCakes - 1
+//       }
+//     case BUY_ICECREAM:
+//       return{
+//         ...state,
+//         numOfIceCream: state.numOfIceCream - 1
+//       }
+//     default: return state
+//   }
+
+// }
+const cakeReducer = (state = initialCakeState, action) =>{
   switch(action.type){
 
     case BUY_CAKE:
@@ -29,11 +61,26 @@ const reducer = (state = initialState, action) =>{
       }
     default: return state
   }
+}
+const iceCreamReducer = (state = initialIceCreamState, action) =>{
+  switch(action.type){
 
+    case BUY_ICECREAM:
+      return{
+        ...state,
+        numOfIceCream: state.numOfIceCream - 1
+      }
+    default: return state
+  }
 }
 //create redux store
 //take reducer function as parameter
-const store = redux.createStore(reducer)
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer
+})
+const store = redux.createStore(rootReducer)
+
 //return state
 console.log('Initial state', store.getState());
 //set up listener whenever there is a change in state it'll console the state
@@ -42,6 +89,8 @@ const unsubscribe = store.subscribe(()=> console.log('Updated state', store.getS
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 
 unsubscribe()
 
